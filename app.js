452b35a89dab9e5846b28eea592ca84e8dc7e9ef
adam <hamer88@hotmail.co.uk>
2017-05-09 21:08:22 +0100

    added app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var useragent = require('express-useragent')
+
+var app = express();
+
+app.use(useragent.express())
+
+app.get('/api/whoami', function(req, res, next){
+  var software = req.useragent.browser + ' and ' + req.useragent.os;
+  res.json({"IP": req.ip,
+            "Language": req.acceptsLanguages()[0],
+            "Software": software})
+})
+
+app.listen(3000, function(){
+  console.log('Server running')
+});
